4b5efaeb6a5f605a68e01b0db1330d014372b147
Nilay-09 <nilaybhotmange2002@gmail.com>
2024-12-31 00:02:52 +0530

    end of bug fixes

--- src/app/api/reports/create/route.ts
+++ src/app/api/reports/create/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
-import { ReportType } from "@prisma/client";
+
+type ReportType = "EMERGENCY"| "NON_EMERGENCY";
 
 export async function POST(request: Request) {
   try {
